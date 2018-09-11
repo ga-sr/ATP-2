6c6b99595e980c3e8e4fe899ae4b42998ee6fc3d
ga-sr <39207023+ga-sr@users.noreply.github.com>
2018-09-11 20:23:35 -0300

    Rename TPP01-Gabriel Rocha-Luana Ferreira.c to TPP01-Manipulacao de polinomios.c

--- TPP01-Manipulacao de polinomios.c
+++ TPP01-Manipulacao de polinomios.c
@@ -1,4 +1,4 @@
-#include <stdio.h>          //TTP01 :SISTEMA PARA MANIPULAÇÃO POLINOMIAL
+#include <stdio.h>          //TTP01 :SISTEMA PARA MANIPULAÃ‡ÃƒO POLINOMIAL
 #include <stdlib.h>
 #define MAX1 20      //maximo de grau
 #include <locale.h>  //para o compilador ler acentos
@@ -25,7 +25,7 @@ int Leitura_Grau_Polinomio(void) {
 	int dim, continua;
 	do {
 		continua = 0;
-		printf("\n\nInsira o  maior grau do polinômio  (Entre 1 e 20): ");
+		printf("\n\nInsira o  maior grau do polinÃ´mio  (Entre 1 e 20): ");
 		scanf("%d",&dim);
 
 		if ( (dim <= 0) || (dim > MAX1) ) continua = 1;
@@ -41,7 +41,7 @@ void Associando_Valores_Array(float *ptr,int Dim)
     float coef;
     zerando_vetores(ptr,Dim);
 
-    do      // forçando o usuário a digitar um valor diferente de zero para o maior grau
+    do      // forÃ§ando o usuÃ¡rio a digitar um valor diferente de zero para o maior grau
     {
     	continua=0;
     	printf("\n\nDigite o coeficiente diferente de zero para o grau %d: ",Dim);
@@ -58,7 +58,7 @@ void Associando_Valores_Array(float *ptr,int Dim)
 
     while(grau !=0)
     {
-    	printf("\n\nDigite o coeficiente e o grau separados por espaço: ");
+    	printf("\n\nDigite o coeficiente e o grau separados por espaÃ§o: ");
     	scanf("%f %d",&coef,&grau);
 
     	Alocar_Polinomio(ptr,coef,grau);
@@ -104,14 +104,14 @@ int Menu (void)
 	int op;
 
 	printf("\n\n                 MENU\n\n");
-	printf("[1] - Somar Polinômios.\n\n");
-	printf("[2] - Multiplicar Polinômios.\n\n");
-	printf("[3] - Dividir Polinômios.\n\n");
-	printf("[4] - Calcular o Polinômio num ponto x específico.\n\n");
-	printf("[5] - Multiplicar o polinômio por uma constante K.\n\n");
-	printf("[6] - Derivar um polinômio.\n\n");
-	printf("[7] - Integrar um polinômio.\n\n");
-	printf("[8] - Determinar o grau e o número de termos do polinômio.\n\n");
+	printf("[1] - Somar PolinÃ´mios.\n\n");
+	printf("[2] - Multiplicar PolinÃ´mios.\n\n");
+	printf("[3] - Dividir PolinÃ´mios.\n\n");
+	printf("[4] - Calcular o PolinÃ´mio num ponto x especÃ­fico.\n\n");
+	printf("[5] - Multiplicar o polinÃ´mio por uma constante K.\n\n");
+	printf("[6] - Derivar um polinÃ´mio.\n\n");
+	printf("[7] - Integrar um polinÃ´mio.\n\n");
+	printf("[8] - Determinar o grau e o nÃºmero de termos do polinÃ´mio.\n\n");
 	scanf("%d",&op);
 
 	return(op);
@@ -121,14 +121,14 @@ void Soma_Polinomio(float *ptr ,float *ptr2,float *ptr3,int dim1,int dim2)
 {
 	int i;
 
-        if(dim1<=dim2)      //se o primeiro vetor é menor que o segundo
+        if(dim1<=dim2)      //se o primeiro vetor Ã© menor que o segundo
         {
 
         	zerando_vetores(ptr3,dim1);
         	for(i=0;i<=dim1;i++)
                 *(ptr3+i)= *(ptr+i) + *(ptr2+i);    // percorremos ele inteiro fazendo a soma com a outro vetor
 
-            for(i;i<=dim2;i++)      // quando esse vetor menor acaba,atribuimos as posições restantes do maior vetor para o vetor soma
+            for(i;i<=dim2;i++)      // quando esse vetor menor acaba,atribuimos as posiÃ§Ãµes restantes do maior vetor para o vetor soma
             	*(ptr3+i)= *(ptr2+i);
         }
 
@@ -136,7 +136,7 @@ void Soma_Polinomio(float *ptr ,float *ptr2,float *ptr3,int dim1,int dim2)
         {
         	zerando_vetores(ptr3,dim2);
         	for(i=0;i<=dim2;i++)
-                *(ptr3+i)= *(ptr+i) + *(ptr2+i);    // mesmo procedimento, porém aqui dim1 > dim2
+                *(ptr3+i)= *(ptr+i) + *(ptr2+i);    // mesmo procedimento, porÃ©m aqui dim1 > dim2
 
             for(i;i<=dim1;i++)   // preencher o restante do vetor
             	*(ptr3+i)= *(ptr+i);
@@ -147,7 +147,7 @@ void Soma_Polinomio(float *ptr ,float *ptr2,float *ptr3,int dim1,int dim2)
 
     void Multiplicacao_Polinomio(float *ptr,float *ptr2,float *ptr3,int dim1,int dim2)
     {
-    int Dim_final=dim1 + dim2, aux; //Dim final= dimensão do vetor multiplicado
+    int Dim_final=dim1 + dim2, aux; //Dim final= dimensÃ£o do vetor multiplicado
 
     for(Dim_final;Dim_final >=0;Dim_final--)
     	*(ptr3+Dim_final)=0;
@@ -176,18 +176,18 @@ void Soma_Polinomio(float *ptr ,float *ptr2,float *ptr3,int dim1,int dim2)
     }
 }
 
-float Calculo_Polinomio_x ( float *ptr, int Dim )  // cálculo de um polinômio num ponto x específico
+float Calculo_Polinomio_x ( float *ptr, int Dim )  // cÃ¡lculo de um polinÃ´mio num ponto x especÃ­fico
 {
 	int i=0;
 	float ponto,soma=0.0;
 
-	printf("\n\nDigite o ponto (x) específico para o cálculo do polinômio:");
+	printf("\n\nDigite o ponto (x) especÃ­fico para o cÃ¡lculo do polinÃ´mio:");
 	scanf("%f",&ponto);
 
 	for(i=0;i<=Dim;i++)
 		soma=soma + pow(ponto,i) * ptr[i] ;
 
-    return (soma);      // retornando o valor total da operação
+    return (soma);      // retornando o valor total da operaÃ§Ã£o
 }
 
 void Multiplicar_Polinomio_Const_K (float *ptr,int Dim,float *ptr2)   //case 5
@@ -195,7 +195,7 @@ void Multiplicar_Polinomio_Const_K (float *ptr,int Dim,float *ptr2)   //case 5
 	float K;
 
 	zerando_vetores(ptr2,Dim);
-	printf("\n\nDigite a constante K para multiplicar o polinômio: ");
+	printf("\n\nDigite a constante K para multiplicar o polinÃ´mio: ");
 	scanf("%f",&K);
 
 	for(Dim;Dim>=0;Dim--)
@@ -214,7 +214,7 @@ void Derivar_Polinomio (float *ptr,int Dim,float *ptr2)
 
 	for(Dim;Dim>=0;Dim--)
 	{
-        if( Dim !=0)  // evitando fazer a operação em valores nulos
+        if( Dim !=0)  // evitando fazer a operaÃ§Ã£o em valores nulos
         	*(ptr2+Dim-1)= Dim *  *(ptr+Dim);
 
         else
@@ -236,35 +236,35 @@ void Integrar_Polinomio (float *ptr,int Dim,float *ptr2)        // integral de x
 
 int main()
 {
-    int n1,n2,n3,opcao,alternativa,continua=1; // variáveis auxiliares
+    int n1,n2,n3,opcao,alternativa,continua=1; // variÃ¡veis auxiliares
     float poli1[MAX1],poli2[MAX1],poli3[MAX1],poli_final[MAX1];
     setlocale (LC_ALL,"Portuguese");
 
-    printf("      TTP01 - Sistema de manipulação de polinômios");
+    printf("      TTP01 - Sistema de manipulaÃ§Ã£o de polinÃ´mios");
 
-    printf("\n\n\n\nINSTRUÇÕES:     Primeiro digite o maior grau de seu polinômio.");
+    printf("\n\n\n\nINSTRUÃ‡Ã•ES:     Primeiro digite o maior grau de seu polinÃ´mio.");
     printf("\n\n    Depois digite o coeficiente real para esse respectivo grau.");
-    printf("\n\n    Então após esse procedimento,digite um valor para o coeficiente real, aperte espaço e digite o grau correspondente a esse coeficiente real.");
-    printf("\n\n    Lembrando que a inserção pára quando você digita zero na posição do grau.");
-    printf("\n\n    Ex: Para o polinômio 3x^2 + 5,digito 2 para o maior grau, 3 para o coeficiente real associado a esse maior grau, e 5 0 para o coeficiente 5 na posição zero.");
-    printf("\n\n    Repita o mesmo procedimento para os outros dois polinômios.\n\n");
+    printf("\n\n    EntÃ£o apÃ³s esse procedimento,digite um valor para o coeficiente real, aperte espaÃ§o e digite o grau correspondente a esse coeficiente real.");
+    printf("\n\n    Lembrando que a inserÃ§Ã£o pÃ¡ra quando vocÃª digita zero na posiÃ§Ã£o do grau.");
+    printf("\n\n    Ex: Para o polinÃ´mio 3x^2 + 5,digito 2 para o maior grau, 3 para o coeficiente real associado a esse maior grau, e 5 0 para o coeficiente 5 na posiÃ§Ã£o zero.");
+    printf("\n\n    Repita o mesmo procedimento para os outros dois polinÃ´mios.\n\n");
     printf("-------------------------------------------------------------------------------\n\n\n");
     system("pause");
     system("cls");
 
-    printf("\n\nPolinômio 1:");
+    printf("\n\nPolinÃ´mio 1:");
     n1=Leitura_Grau_Polinomio();
     Associando_Valores_Array(poli1,n1);
     printf("\n\n");
     Imprimir_polinomio(poli1,n1);
 
-    printf("\n\n Polinômio 2:");
+    printf("\n\n PolinÃ´mio 2:");
     n2=Leitura_Grau_Polinomio();
     Associando_Valores_Array(poli2,n2);
     printf("\n\n");
     Imprimir_polinomio(poli2,n2);
 
-    printf("\n\n Polinômio 3:");
+    printf("\n\n PolinÃ´mio 3:");
     n3=Leitura_Grau_Polinomio();
     Associando_Valores_Array(poli3,n3);
     printf("\n\n");
@@ -281,26 +281,26 @@ int main()
 
     	system("cls");
 
-    	printf("\n\n Polinômio 1:");
+    	printf("\n\n PolinÃ´mio 1:");
     	printf("\n\n");
     	Imprimir_polinomio(poli1,n1);
-    	printf("\n\n Polinômio 2:");
+    	printf("\n\n PolinÃ´mio 2:");
     	printf("\n\n");
     	Imprimir_polinomio(poli2,n2);
-    	printf("\n\n Polinômio 3:");
+    	printf("\n\n PolinÃ´mio 3:");
     	printf("\n\n");
     	Imprimir_polinomio(poli3,n3);
 
     	switch(opcao)
     	{
-            case 1:     // soma polinômio
+            case 1:     // soma polinÃ´mio
 
-            printf("\n\n[1] - Se deseja somar o polinômio 1 com o 2.");
-            printf("\n\n[2] - Se deseja somar o polinômio 1 com o 3.");
-            printf("\n\n[3] - Se deseja somar o polinômio 2 com o 3.");
-            printf("\n\n[4] - Se deseja somar o polinômio 1 com ele mesmo.");
-            printf("\n\n[5] - Se deseja somar o polinômio 2 com ele mesmo.");
-            printf("\n\n[6] - Se deseja somar o polinômio 3 com ele mesmo.\n");
+            printf("\n\n[1] - Se deseja somar o polinÃ´mio 1 com o 2.");
+            printf("\n\n[2] - Se deseja somar o polinÃ´mio 1 com o 3.");
+            printf("\n\n[3] - Se deseja somar o polinÃ´mio 2 com o 3.");
+            printf("\n\n[4] - Se deseja somar o polinÃ´mio 1 com ele mesmo.");
+            printf("\n\n[5] - Se deseja somar o polinÃ´mio 2 com ele mesmo.");
+            printf("\n\n[6] - Se deseja somar o polinÃ´mio 3 com ele mesmo.\n");
             scanf("%d",&alternativa);
 
 
@@ -379,14 +379,14 @@ int main()
 
                 break;
 
-            case 2:     // multiplicar o polinômio
+            case 2:     // multiplicar o polinÃ´mio
 
-            printf("\n\n[1] - Se deseja multiplicar o polinômio 1 com o 2.");
-            printf("\n\n[2] - Se deseja multiplicar o polinômio 1 com o 3.");
-            printf("\n\n[3] - Se deseja multiplicar o polinômio 2 com o 3.");
-            printf("\n\n[4] - Se deseja multiplicar o polinômio 1 com ele mesmo.");
-            printf("\n\n[5] - Se deseja multiplicar o polinômio 2 com ele mesmo.");
-            printf("\n\n[6] - Se deseja multiplicar o polinômio 3 com ele mesmo.\n");
+            printf("\n\n[1] - Se deseja multiplicar o polinÃ´mio 1 com o 2.");
+            printf("\n\n[2] - Se deseja multiplicar o polinÃ´mio 1 com o 3.");
+            printf("\n\n[3] - Se deseja multiplicar o polinÃ´mio 2 com o 3.");
+            printf("\n\n[4] - Se deseja multiplicar o polinÃ´mio 1 com ele mesmo.");
+            printf("\n\n[5] - Se deseja multiplicar o polinÃ´mio 2 com ele mesmo.");
+            printf("\n\n[6] - Se deseja multiplicar o polinÃ´mio 3 com ele mesmo.\n");
             scanf("%d",&alternativa);
 
                 if(alternativa==1)   // 1 com 2
@@ -435,64 +435,64 @@ int main()
 
                 case 3:
 
-                printf("\n\n Esta operação não foi implementada!!!");
+                printf("\n\n Esta operaÃ§Ã£o nÃ£o foi implementada!!!");
 
                 break;
 
 
-            case 4: // cálculo num ponto x específico
+            case 4: // cÃ¡lculo num ponto x especÃ­fico
 
-            printf("\n\nQual polinômio você quer calcular num ponto x específico? 1,2 ou 3? ");
+            printf("\n\nQual polinÃ´mio vocÃª quer calcular num ponto x especÃ­fico? 1,2 ou 3? ");
             scanf("%d",&alternativa);
 
             if(alternativa==1)
-            	printf("\n\nO valor do polinômio num ponto  específico é: %.2f",Calculo_Polinomio_x(poli1,n1) );
+            	printf("\n\nO valor do polinÃ´mio num ponto  especÃ­fico Ã©: %.2f",Calculo_Polinomio_x(poli1,n1) );
 
             if(alternativa==2)
-            	printf("\n\nO valor do polinômio num ponto  específico é: %.2f",Calculo_Polinomio_x(poli2,n2) );
+            	printf("\n\nO valor do polinÃ´mio num ponto  especÃ­fico Ã©: %.2f",Calculo_Polinomio_x(poli2,n2) );
 
             if(alternativa==3)
-            	printf("\n\nO valor do polinômio num ponto  específico é: %.2f",Calculo_Polinomio_x(poli3,n3) );
+            	printf("\n\nO valor do polinÃ´mio num ponto  especÃ­fico Ã©: %.2f",Calculo_Polinomio_x(poli3,n3) );
 
             break;
 
-            case 5: // multiplicação por uma constante K
+            case 5: // multiplicaÃ§Ã£o por uma constante K
 
-            printf("\n\nQual polinômio você quer multiplicar por uma constante K? 1,2 ou 3? ");
+            printf("\n\nQual polinÃ´mio vocÃª quer multiplicar por uma constante K? 1,2 ou 3? ");
             scanf("%d",&alternativa);
 
             if(alternativa==1)
             {
             	Multiplicar_Polinomio_Const_K(poli1,n1,poli_final);
-            	printf("\nO polinômio multiplicado pela constante escolhida é:\n\n");
+            	printf("\nO polinÃ´mio multiplicado pela constante escolhida Ã©:\n\n");
             	Imprimir_polinomio(poli_final,n1);
             }
 
             if(alternativa==2)
             {
             	Multiplicar_Polinomio_Const_K(poli2,n2,poli_final);
-            	printf("\nO polinômio multiplicado pela constante escolhida é:\n\n");
+            	printf("\nO polinÃ´mio multiplicado pela constante escolhida Ã©:\n\n");
             	Imprimir_polinomio(poli_final,n2);
             }
 
             if(alternativa==3)
             {
             	Multiplicar_Polinomio_Const_K(poli3,n3,poli_final);
-            	printf("\nO polinômio multiplicado pela constante escolhida é:\n\n");
+            	printf("\nO polinÃ´mio multiplicado pela constante escolhida Ã©:\n\n");
             	Imprimir_polinomio(poli_final,n3);
             }
 
             break;
 
-            case 6: // derivar polinômios
+            case 6: // derivar polinÃ´mios
 
-            printf("\n\n Qual polinômio você deseja derivar? 1,2 ou 3? ");
+            printf("\n\n Qual polinÃ´mio vocÃª deseja derivar? 1,2 ou 3? ");
             scanf("%d",&alternativa);
 
             if(alternativa==1)
             {
             	Derivar_Polinomio(poli1,n1,poli_final);
-            	printf("\nO polinômio derivado é:\n");
+            	printf("\nO polinÃ´mio derivado Ã©:\n");
             	printf("\n");
             	Imprimir_polinomio(poli_final,n1-1);
 
@@ -501,16 +501,16 @@ int main()
             if(alternativa==2)
             {
             	Derivar_Polinomio(poli2,n2,poli_final);
-            	printf("\nO polinômio derivado é:\n");
+            	printf("\nO polinÃ´mio derivado Ã©:\n");
             	printf("\n");
-                    Imprimir_polinomio(poli_final,n2-1);    // pois o maior grau na derivada será n2-1
+                    Imprimir_polinomio(poli_final,n2-1);    // pois o maior grau na derivada serÃ¡ n2-1
 
                 }
 
                 if(alternativa==3)
                 {
                 	Derivar_Polinomio(poli3,n3,poli_final);
-                	printf("\nO polinômio derivado é:\n");
+                	printf("\nO polinÃ´mio derivado Ã©:\n");
                 	printf("\n");
                 	Imprimir_polinomio(poli_final,n3-1);
 
@@ -518,15 +518,15 @@ int main()
 
                 break;
 
-            case 7:     // integrar polinômio
+            case 7:     // integrar polinÃ´mio
 
-            printf("\n\nQual polinômio você deseja integrar?  1,2 ou 3? ");
+            printf("\n\nQual polinÃ´mio vocÃª deseja integrar?  1,2 ou 3? ");
             scanf("%d",&alternativa);
 
             if(alternativa==1)
             {
             	Integrar_Polinomio(poli1,n1,poli_final);
-            	printf("\nO polinômio integrado é:\n\n");
+            	printf("\nO polinÃ´mio integrado Ã©:\n\n");
             	Imprimir_polinomio(poli_final,n1+1);
             	printf(" + constante c");
 
@@ -535,7 +535,7 @@ int main()
             if(alternativa==2)
             {
             	Integrar_Polinomio(poli2,n2,poli_final);
-            	printf("\nO polinômio integrado é:\n\n");
+            	printf("\nO polinÃ´mio integrado Ã©:\n\n");
             	Imprimir_polinomio(poli_final,n2+1);
             	printf(" + constante c");
             }
@@ -543,7 +543,7 @@ int main()
             if(alternativa==3)
             {
             	Integrar_Polinomio(poli3,n3,poli_final);
-            	printf("\nO polinômio integrado é:\n\n");
+            	printf("\nO polinÃ´mio integrado Ã©:\n\n");
             	Imprimir_polinomio(poli_final,n3+1);
             	printf(" + constante c");
             }
@@ -552,22 +552,22 @@ int main()
 
             case 8: // quantidade de termos
 
-            printf("\n\nQual polinômio você quer saber o grau?\n 1,2 ou 3? ");
+            printf("\n\nQual polinÃ´mio vocÃª quer saber o grau?\n 1,2 ou 3? ");
             scanf("%d",&alternativa);
 
             if(alternativa==1)
             {
-            	printf("\n\nO grau do polinômio é %d",n1);
+            	printf("\n\nO grau do polinÃ´mio Ã© %d",n1);
             }
 
             if(alternativa==2)
             {
-            	printf("\n\nO grau do polinômio é %d",n2);
+            	printf("\n\nO grau do polinÃ´mio Ã© %d",n2);
             }
 
             if(alternativa==3)
             {
-            	printf("\n\nO grau do polinômio é %d",n3);
+            	printf("\n\nO grau do polinÃ´mio Ã© %d",n3);
             }
 
             break;
@@ -575,7 +575,7 @@ int main()
         }// switch
 
         printf("\n\n\n Digite 1 para executar o programa novamente ou zero para sair");
-        printf("\n\n(Se você escolher a opção 1,os polinômios digitados continuam salvos.): ");
+        printf("\n\n(Se vocÃª escolher a opÃ§Ã£o 1,os polinÃ´mios digitados continuam salvos.): ");
         scanf("%d",&continua);
         system("cls");
     }//while
